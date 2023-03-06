cba6b221527bdf67af139a7ae3104606d64db437
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2023-03-06 14:57:38 +0300

    Deploy Message

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ app.include_router(auth.router)
 
 @app.get("/")
 def root():
-    return {"message": " Bay Kemal AWS Code Deploy Duzgun Bir Sekilde Calisiyor!."}
+    return {"message": " Vedat Abi AWS Code Deploy Calisiyor :)"}
 
