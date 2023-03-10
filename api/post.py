09cffb1f41c9f0176efb6a9c00ad052042ea0b58
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2023-03-10 11:39:39 +0300

    Post Methods Token Authorization

--- api/post.py
+++ api/post.py
@@ -12,27 +12,27 @@ router = APIRouter(
 )
 
 @router.get("/get", response_model= List[PostOut])
-async def get_posts(db: Session = Depends(get_db)):
+async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
     return await services.get_posts(db)
     
 
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model= PostOut)
-async def create_posts(post: CreatePost, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def create_posts(post: CreatePost, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
     return await services.create_posts(post, db)
 
 @router.get("/get/{id}", response_model= PostOut)
-async def get_post(id: int, db: Session = Depends(get_db)):
+async def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     return await services.get_post(id, db)
 
 @router.delete("/delete/{id}", status_code= status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int, db: Session = Depends(get_db)):
+async def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     return await services.delete_post(id, db)
 
 @router.put("/update/{id}", response_model= PostOut)
-async def update_post(id: int, updated_post: CreatePost, db: Session = Depends(get_db)):
+async def update_post(id: int, updated_post: CreatePost, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
     return await services.update_post(id, updated_post, db)
--- schemas/post.py
+++ schemas/post.py
@@ -12,6 +12,7 @@ class CreatePost(PostBase):
 class PostOut(PostBase):
     id: int
     created_at: datetime
-    
+    owner_id: int
+
     class Config:
         orm_mode = True
