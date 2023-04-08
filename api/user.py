c2e035abe329af8ae2ff7c3e6a1e4d1794e766b4
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2023-04-08 17:30:27 +0300

    Minoe Changes

--- api/user.py
+++ api/user.py
@@ -16,10 +16,10 @@ router = APIRouter(
 @router.get("/get/{id}", response_model=ProfileOut)
 async def get_profile(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-    return await services.get_user(id, db)
+    return await services.get_profile(id, db)
 
 
 @router.put("/update/{id}", response_model=UserSchema)
 async def update_profile(id: int, updated_user: UserSchema, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-    return await services.update_user(id, updated_user, db)
+    return await services.update_profile(id, updated_user, db)
