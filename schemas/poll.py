113ba093afcebb81097056254d9f3a5b98dc7517
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2023-04-30 20:08:21 +0300

    Minor Change

--- schemas/poll.py
+++ schemas/poll.py
@@ -6,8 +6,6 @@ class PollBase(BaseModel):
     post_id: int = Field(default=None)
     item: str = Field(default=None)
     isChosen: bool = Field(default=None)
-    votes: int = Field(default=None)
-    percentage: int = Field(default=None)
 
     class Config:
         orm_mode = True
