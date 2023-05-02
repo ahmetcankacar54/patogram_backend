76d17c5f81773d2498d7b4c07c0bad6535b64faa
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2023-05-02 13:49:50 +0300

    Vote API Reference Changed

--- api/vote.py
+++ api/vote.py
@@ -12,7 +12,7 @@ router = APIRouter(
 )
 
 
-@router.post("/", status_code=status.HTTP_200_OK)
+@router.post("", status_code=status.HTTP_200_OK)
 async def vote(vote: AddVote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     return await services.vote(vote, current_user.id, db)
